86fd85cd9ac140863efae229c6f30a1de75d86c0
TheVanadium <53013571+TheVanadium@users.noreply.github.com>
2025-04-15 16:26:56 -0700

    test: write tests for projects class

---
+++ test_datatypes.py
@@ -0,0 +1,44 @@
+import pytest
+from hypothesis import given, strategies as st
+from datatypes import Project
+
+
+@pytest.mark.parametrize(
+    "name, team_number, skills_dict, original_name",
+    [
+        ("Project", 1, {"skill1": 1, "skill2": 0}, "Project"),
+        ("Project_1", 2, {"skill3": 1, "skill4": 0}, "Project"),
+        ("XAOE", 3, {"skill5": 1, "skill4": 0}, "XAOE"),
+        ("XAOE_2", 3, {"skill1": 1, "skill2": 0}, "XAOE"),
+    ],
+)
+def test_project_init(name, team_number, skills_dict, original_name):
+    project = Project(name, team_number, skills_dict)
+
+    assert project.name == name
+    assert project.original_name == original_name
+    assert project.skills_dict == skills_dict
+    assert project.team_number == team_number
+    assert project.assigned_lab == ""
+    assert project.assigned_students == []
+    assert project.team_capacity == 0
+
+
+@given(
+    name=st.text(min_size=1),
+    team_number=st.integers(min_value=1, max_value=10),
+    skills_dict=st.dictionaries(
+        keys=st.text(min_size=1),
+        values=st.integers(min_value=0, max_value=5),
+        min_size=1,
+    ),
+)
+def test_project_init_hypothesis(name, team_number, skills_dict):
+    project = Project(name, team_number, skills_dict)
+
+    assert project.name == name
+    assert project.skills_dict == skills_dict
+    assert project.team_number == team_number
+    assert project.assigned_lab == ""
+    assert project.assigned_students == []
+    assert project.team_capacity == 0
