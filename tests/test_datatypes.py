438d207a42d73fb54beb0c1e10d580aec75899d5
TheVanadium <53013571+TheVanadium@users.noreply.github.com>
2025-04-16 18:32:40 -0700

    test: move test_datatypes.py into tests folder
    whoops

--- tests/test_datatypes.py
+++ tests/test_datatypes.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import pytest
 from hypothesis import given, strategies as st
 from datatypes import Project
