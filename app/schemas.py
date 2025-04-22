4a83895abca28bca65f141e36adae2535e6acc57
Avash Adhikari <avashraj328@gmail.com>
2025-04-22 02:01:50 -0700

    feat: add request and response schemas for team formation route

--- app/schemas.py
+++ app/schemas.py
@@ -3,3 +3,14 @@ API Request and Response schemas Definitions based on Pydantic Models
 
 These models are used to define the structure of data that is sent and received through the API.
 """
+
+from models import Project, Student, Team
+from typing import List
+from pydantic import BaseModel
+
+class FormTeamRequest(BaseModel):
+    students: List[Student]
+    projects: List[Project]
+
+class FormTeamResponse(BaseModel):
+    teams: List[Team]
